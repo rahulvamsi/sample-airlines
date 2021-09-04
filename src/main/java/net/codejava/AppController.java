069d824339f668cb1512a145525c5e2881a2a473
Debraj68 <mukherjee.debraj861@outlook.com>
2021-09-04 12:24:19 +0530

    added changes in the jenkinfile1

--- src/main/java/net/codejava/AppController.java
+++ src/main/java/net/codejava/AppController.java
@@ -8,8 +8,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
+@RestController
+@RequestMapping("/home")
 public class AppController {
 
 	@Autowired
