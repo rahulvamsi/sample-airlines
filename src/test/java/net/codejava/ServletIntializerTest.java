e46f137af8dbcb60112ef538cd81dd6d328bf3c9
rahulvamsi <p.rahulvamsi@gmail.com>
2021-09-06 17:39:10 +0530

    updated test12

---
+++ src/test/java/net/codejava/ServletIntializerTest.java
@@ -0,0 +1,19 @@
+package net.codejava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class  ServletIntializerTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+    
+    
+
+}
+
+
+
